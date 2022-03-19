0a0323c1d54187a49a6311979d4d8e88d4df6ca8
Fabricio Bracht <f.bracht@ai-systems.ca>
2022-03-18 19:28:44 -0700

    Added remote candidate handler

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use std::pin::Pin;
 use std::sync::Arc;
 use tokio::io::{AsyncReadExt, Result};
 use tokio::net::{TcpStream, UdpSocket};
-use tokio::sync::mpsc;
+use tokio::sync::mpsc::{self, Receiver};
 
 use util::Conn;
 use webrtc_ice::agent::agent_config::AgentConfig;
@@ -18,27 +18,18 @@ use webrtc_ice::udp_network::UDPNetwork;
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    let (tx, mut rx) = mpsc::channel(1);
+    let (tx_auth, mut rx_auth) = mpsc::channel(1);
+    let (tx_cand, mut rx_cand) = mpsc::channel(1);
+
     let control_arg = std::env::args().take(2).collect::<Vec<String>>();
     let _use_mux = false;
     let is_controlling = control_arg[1].parse::<bool>().unwrap();
     let _remote_port = 4000;
-
     let ice_agent = create_ice_agent(is_controlling).await.unwrap();
-    ice_agent.on_candidate(handle_candidate()).await;
-    let (ice_done_tx, _ice_done_rx) = mpsc::channel::<()>(1);
 
-    ice_agent
-        .on_connection_state_change(Box::new(move |c: ConnectionState| {
-            println!("ICE Connection State has changed: {}", c);
-            if c == ConnectionState::Failed {
-                let _ = ice_done_tx.try_send(());
-            }
-            Box::pin(async move {})
-        }))
-        .await;
     // Get the local auth details and send to remote peer
     let (local_ufrag, local_pwd) = ice_agent.get_local_user_credentials().await;
+
     println!("Connecting");
     let connection = if is_controlling {
         TcpStream::connect("127.0.0.1:9001").await?
@@ -46,36 +37,18 @@ async fn main() -> Result<()> {
         TcpStream::connect("172.30.29.1:9001").await?
     };
 
-    let mut buf = [0; 4096];
+
     let (mut stream, sink) = connection.into_split();
     tokio::spawn(async move {
-        let message_string = format!("{}:{}", local_ufrag, local_pwd);
-        let message = message_string.as_bytes();
-        if !is_controlling {
-            sink.writable().await.unwrap();
-            sink.try_write(message).unwrap();
-        }
-
-        while let Ok(res) = stream.read(&mut buf).await {
-            let bslice = std::str::from_utf8(&buf[..res]).unwrap().to_string();
-            println!("Message: {}", bslice);
-            // let message = std::str::from_utf8(&buf).unwrap();
-            if is_controlling {
-                sink.writable().await.unwrap();
-                sink.try_write(message).unwrap();
-            }
-            tx.send(bslice).await.unwrap();
-        }
+        stream_handler(local_ufrag, local_pwd, is_controlling, sink, stream, tx_auth).await;
     });
 
-    ice_agent.gather_candidates().await.unwrap();
-    println!("Connecting...");
-    let remote_credentials_result = rx.recv().await;
-    let remote_credentials = remote_credentials_result.clone().unwrap().clone();
+    ice_agent.on_candidate(handle_candidate(tx_cand)).await;
+
+    let candidate = rx_cand.recv().await;
 
     let ice_agent2 = Arc::clone(&ice_agent);
-
-    if let Some(s) = remote_credentials_result {
+    if let Some(s) = candidate {
         if let Ok(c) = unmarshal_candidate(&s).await {
             println!("add_remote_candidate: {}", c);
             let c: Arc<dyn Candidate + Send + Sync> = Arc::new(c);
@@ -87,25 +60,74 @@ async fn main() -> Result<()> {
         println!("REMOTE_CAND_CHANNEL done!");
     }
 
+
+    let (ice_done_tx, _ice_done_rx) = mpsc::channel::<()>(1);
+
+    ice_agent
+        .on_connection_state_change(Box::new(move |c: ConnectionState| {
+            println!("ICE Connection State has changed: {}", c);
+            if c == ConnectionState::Failed {
+                let _ = ice_done_tx.try_send(());
+            }
+            Box::pin(async move {})
+        }))
+        .await;
+    
+
+    ice_agent.gather_candidates().await.unwrap();
+    println!("Connecting...");
+    let remote_credentials = remote_auth_handler(rx_auth, ice_agent, is_controlling).await;
+
+    println!("{}", remote_credentials);
+    println!("Finished");
+    Ok(())
+}
+
+async fn remote_auth_handler(mut rx_auth: mpsc::Receiver<String>, ice_agent: Arc<Agent>, is_controlling: bool) -> String {
+    let remote_credentials_result = rx_auth.recv().await;
+    let remote_credentials = remote_credentials_result.clone().unwrap().clone();
+    
     let r = remote_credentials.split(":").take(2).collect::<Vec<&str>>();
     let (remote_ufrag, remote_pwd) = (r[0].to_string(), r[1].to_string());
-
     let (_cancel_tx, cancel_rx) = mpsc::channel(1);
     let _conn: Arc<dyn Conn + Send + Sync> = if is_controlling {
+        println!("Dialing...");
         ice_agent
             .dial(cancel_rx, remote_ufrag, remote_pwd)
             .await
             .unwrap()
     } else {
+        println!("Accepting...");
         ice_agent
             .accept(cancel_rx, remote_ufrag, remote_pwd)
             .await
             .unwrap()
     };
+    remote_credentials
+}
 
-    println!("{}", remote_credentials);
-    println!("Finished");
-    Ok(())
+async fn stream_handler(local_ufrag: String, local_pwd: String, is_controlling: bool, sink: tokio::net::tcp::OwnedWriteHalf, mut stream: tokio::net::tcp::OwnedReadHalf, tx_auth: mpsc::Sender<String>) {
+    let mut buf =  [0u8; 4096];
+    let message_string = format!("{}:{}", local_ufrag, local_pwd);
+    let message = message_string.as_bytes();
+    if !is_controlling {
+        sink.writable().await.unwrap();
+        sink.try_write(message).unwrap();
+    }
+    while let Ok(res) = stream.read(&mut buf).await {
+        let bslice = std::str::from_utf8(&buf[..res]).unwrap().to_string();
+        println!("Message: {}", bslice);
+        // let message = std::str::from_utf8(&buf).unwrap();
+        if bslice.contains(":") {
+            tx_auth.send(bslice).await.unwrap();
+        }
+        if is_controlling {
+            // If controlling, send our auth credentials
+            sink.writable().await.unwrap();
+            sink.try_write(message).unwrap();
+
+        }
+    }
 }
 
 async fn create_ice_agent(_is_controlling: bool) -> Result<Arc<Agent>> {
@@ -127,15 +149,22 @@ async fn create_ice_agent(_is_controlling: bool) -> Result<Arc<Agent>> {
     Ok(ice_agent)
 }
 
-fn handle_candidate<'a>() -> Box<
+fn handle_candidate<'a>(tx_cand: tokio::sync::mpsc::Sender<String>) -> Box<
     dyn FnMut(Option<Arc<dyn Candidate + Send + Sync>>) -> Pin<Box<dyn Future<Output = ()> + Send>>
         + Send
         + Sync,
 > {
     Box::new(move |c: Option<Arc<dyn Candidate + Send + Sync>>| {
+        let tx_cand = tx_cand.clone();
+
         Box::pin(async move {
+            let tx_cand = tx_cand.clone();
             if let Some(c) = c {
                 println!("posting remoteCandidate with {}", c.marshal());
+                tokio::join!(async move {
+                    tx_cand.send(c.marshal()).await.unwrap();
+                });
+                
             }
         })
     })
